2979f7d1b8eb3b58b37c91693b9babec8839d9dc
michael haaf <michael.haaf@gmail.com>
2018-12-05 23:34:11 -0500

    add top feature visualization, results directory

--- source/classifier.py
+++ source/classifier.py
@@ -3,6 +3,7 @@ import pickle
 import os
 import re
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn import metrics
 from sklearn.feature_extraction.text import CountVectorizer
@@ -47,6 +48,23 @@ def get_label_from_file_path(file_path):
         return label.group()
 
 
+# code borrowed from https://medium.com/@aneesha/visualising-top-features-in-linear-svm-with-scikit-learn-and-matplotlib-3454ab18a14d
+def plot_coefficients(classifier, feature_names, top_features=20):
+    coef = classifier.coef_.ravel()
+    top_positive_coefficients = np.argsort(coef)[-top_features:]
+    top_negative_coefficients = np.argsort(coef)[:top_features]
+    top_coefficients = np.hstack([top_negative_coefficients, top_positive_coefficients])
+    # create plot
+    plt.figure(figsize=(15, 5))
+    colors = ['red' if c < 0 else 'blue' for c in coef[top_coefficients]]
+    plt.bar(np.arange(2 * top_features), coef[top_coefficients], color=colors)
+    feature_names = np.array(feature_names)
+    plt.xticks(np.arange(1, 1 + 2 * top_features),
+               feature_names[top_coefficients % len(feature_names)],
+               rotation=60, ha='right')
+    plt.show()
+
+
 if __name__ == "__main__":
 
     book_data_dict = load_data()
@@ -55,7 +73,7 @@ if __name__ == "__main__":
     text_classifier = Pipeline([
         ('vect', CountVectorizer(ngram_range=(1, 2))),
         ('tfidf', TfidfTransformer()),
-        ('clf', SGDClassifier(loss='hinge', penalty='l2')) # see https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
+        ('clf', SGDClassifier(random_state=42, penalty='l1'))
     ])
 
     X_train, X_test, y_train, y_test = train_test_split(processed_data, labels, test_size=0.20)
@@ -64,4 +82,6 @@ if __name__ == "__main__":
     predicted = text_classifier.predict(X_test)
     np.mean(predicted == y_test)
     print(metrics.classification_report(y_test, predicted))
+    plot_coefficients(text_classifier.named_steps['clf'],
+                      text_classifier.named_steps['vect'].get_feature_names())
 
