8f0d302306639e590dda3842ee409fcf3d4e0ba2
michael haaf <michael.haaf@gmail.com>
2018-12-10 23:35:47 -0500

    rename some literature pdfs to be more clear, move plotting utilities to their own class for clarity

--- source/classifier.py
+++ source/classifier.py
@@ -1,9 +1,6 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import itertools
 import operator
 import argparse
-
 
 from sklearn import metrics
 from sklearn.feature_extraction import DictVectorizer
@@ -47,55 +44,6 @@ def features(selected_features, tags_list):
         for feature in selected_features:
             features.append(features_dict[feature].apply(tags))
     return features
-
-
-# code borrowed from https://medium.com/@aneesha/visualising-top-features-in-linear-svm-with-scikit-learn-and-matplotlib-3454ab18a14d
-def plot_coefficients(classifier, feature_names, top_features=20):
-    coef = classifier.coef_.ravel()
-    top_positive_coefficients = np.argsort(coef)[-top_features:]
-    top_negative_coefficients = np.argsort(coef)[:top_features]
-    top_coefficients = np.hstack([top_negative_coefficients, top_positive_coefficients])
-    # create plot
-    plt.figure(figsize=(15, 5))
-    colors = ['red' if c < 0 else 'blue' for c in coef[top_coefficients]]
-    plt.bar(np.arange(2 * top_features), coef[top_coefficients], color=colors)
-    feature_names = np.array(feature_names)
-    plt.xticks(np.arange(1, 1 + 2 * top_features),
-               [str(name) for name in feature_names[top_coefficients % len(feature_names)]],
-               rotation=60, ha='right')
-    plt.show()
-
-
-# code borrowed from https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix'):
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    plt.imshow(cm, interpolation='nearest')
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.tight_layout()
-    plt.show()
 
 
 pipeline = Pipeline([
